3c982e85455d2f15912afa3bb57ec7776f390e40
Ardalan <ardalaan.f@gmail.com>
2023-03-23 22:01:30 +0100

    pong game, paddle class added

---
+++ pong-game/main.py
@@ -0,0 +1,30 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import tkinter as tk
+from turtle import Screen
+from paddle import  Paddle
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+screen.exitonclick()
+
+
